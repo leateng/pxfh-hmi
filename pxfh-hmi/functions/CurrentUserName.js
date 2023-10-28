934e2eb6dfd1cec65856e91a3327ee56b36ff6eb
Teng.Li <leateng@foxmail.com>
2023-10-28 22:29:29 +0800

    update

---
+++ pxfh-hmi/functions/CurrentUserName.js
@@ -0,0 +1,16 @@
+// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="./../../Packages/Beckhoff.TwinCAT.HMI.Framework.12.760.48/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    var Functions;
+    (function (/** @type {globalThis.TcHmi.Functions} */ Functions) {
+        var pxfh_hmi;
+        (function (pxfh_hmi) {
+            function CurrentUserName() {
+                return TcHmi.Server.getCurrentUser();
+            }
+            pxfh_hmi.CurrentUserName = CurrentUserName;
+        })(pxfh_hmi = Functions.pxfh_hmi || (Functions.pxfh_hmi = {}));
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
+TcHmi.Functions.registerFunctionEx('CurrentUserName', 'TcHmi.Functions.pxfh_hmi', TcHmi.Functions.pxfh_hmi.CurrentUserName);
